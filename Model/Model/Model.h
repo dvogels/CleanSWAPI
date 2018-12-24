a3ab226118157718e8b827118d1ef411e3a7df1c
Dieter Vogels <dieter.vogels@jidoka.be>
2018-12-24 09:03:27 +0100

    Added Model and Services Frameworks

---
+++ Model/Model/Model.h
@@ -0,0 +1,19 @@
+//
+//  Model.h
+//  Model
+//
+//  Created by Dieter Vogels on 24/12/2018.
+//  Copyright © 2018 Jidoka BVBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Model.
+FOUNDATION_EXPORT double ModelVersionNumber;
+
+//! Project version string for Model.
+FOUNDATION_EXPORT const unsigned char ModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Model/PublicHeader.h>
+
+
---
+++ Services/Services/Services.h
@@ -0,0 +1,19 @@
+//
+//  Services.h
+//  Services
+//
+//  Created by Dieter Vogels on 24/12/2018.
+//  Copyright © 2018 Jidoka BVBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Services.
+FOUNDATION_EXPORT double ServicesVersionNumber;
+
+//! Project version string for Services.
+FOUNDATION_EXPORT const unsigned char ServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Services/PublicHeader.h>
+
+
